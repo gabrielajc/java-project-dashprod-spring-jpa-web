7887fbe422147661f3f95971cf664aacd1f62d94
Gabriela Clemente <gabeclementej@gmail.com>
2025-11-03 14:54:28 -0300

    feat: tratamento de excessoes com retorno JSON na configuracao do filterChain, metodo jwtDecoder e Enconder

--- src/main/java/com/br/dashProd/security/SecutiryConfig.java
+++ src/main/java/com/br/dashProd/security/SecutiryConfig.java
@@ -1,5 +1,6 @@
 package com.br.dashProd.security;
 
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.crypto.SecretKey;
@@ -17,25 +19,44 @@ import java.nio.charset.StandardCharsets;
 public class SecutiryConfig {
 
     @Bean
-    private SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/dashProd/v1/**").authenticated())
-                .oauth2ResourceServer(resource -> resource.jwt(
-                        jwtConfigurer -> {
-
-                        }
-                ))
+                .oauth2ResourceServer(resource -> resource.jwt(jwtConfigurer -> {}))
+                .exceptionHandling(e -> e
+                                .authenticationEntryPoint((request, response, authException) ->
+                                {
+                                    response.setStatus(401);
+                                    response.setContentType("application/json");
+                                    response.getWriter().write(
+                                            "{\"status\":401, \"message\": \"" + authException.getMessage() + "\" }");
+                                })
+                                .accessDeniedHandler((request, response, accessDeniedException) ->
+                                {
+                                    response.setStatus(403);
+                                    response.setContentType("application/json");
+                                    response.getWriter().write(
+                                            "{\"status\":403, \"message\": \"" + accessDeniedException.getMessage() + "\" }");
+                                } )
+                        )
                 .build();
+
     }
 
     @Bean
-    private NimbusJwtDecoder jwtDecoder(@Value("${JWT_SECRET}") String secret){
+    public NimbusJwtDecoder jwtDecoder(@Value("${JWT_SECRET}") String secret){
        SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
        return NimbusJwtDecoder.withSecretKey(secretKey).build();
+    }
+
+    @Bean
+    public NimbusJwtEncoder jwtEncoder(@Value("${JWT_SECRET}") String secret){
+        SecretKey secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        return new NimbusJwtEncoder(new ImmutableSecret<>(secretKey));
     }
 
 }
